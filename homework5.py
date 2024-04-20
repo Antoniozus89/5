b705a38dca8f05c36f68045161f5847948283699
Antoniozus89 <166949414+Antoniozus89@users.noreply.github.com>
2024-04-20 14:18:25 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,12 @@
+my_list = ['арбуз ', 'банан ','киви ', 'яблоко', 'груша']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2: 5])
+my_list[2] = 'манго'
+print(my_list)
+my_dict = {'арбуз' :'watermelon', 'банан' :'banana', 'киви' :'kiwi', 'яблоко' :'apple', 'груша' :'pear'}
+print(my_dict)
+print(my_dict . get('арбуз'))
+my_dict['арбуз'] = 1
+print(my_dict)
